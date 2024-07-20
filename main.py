2376c109df26bb9c5f8a9fcce122daa7be9042da
Микола <andreygavryliuk300811@gmail.com>
2024-07-20 19:56:05 +0300

    ...

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
+from random import randint
 window = pygame.display.set_mode((700, 500))
 pygame.display.set_caption("Shoter")
 background = pygame.transform.scale(pygame.image.load("castle.webp"), (700, 500)) 
 game = True
 clock = pygame.time.Clock()
+height = 500
 
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, image, x, y, speed, size_x,size_y):
@@ -16,7 +18,7 @@ class GameSprite(pygame.sprite.Sprite):
     def draw(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 player = GameSprite('rocket.png', 20, 408, 4,50,90)
-
+enemies=[GameSprite('ufo.png', randint(0,620), -50, randint(2), 80, 50) for i in range(5)]
 pygame.mixer.init()
 pygame.mixer.music.load('baby.mp3')
 pygame.mixer.music.play()
@@ -30,12 +32,20 @@ while game:
         player.rect.x -= 1
     if keys[pygame.K_RIGHT] and player.rect.x < 645:
         player.rect.x += 1
-    
+
 
     
 
     
     window.blit(background,(0,0)  )
     player.draw()
+    
+    for enemy in enemies:
+        enemy.draw()
+        enemy.rect.y += enemy.speed
+        if enemy.rect.y > height:
+            enemy.rect.y = -50
+            enemy.rect.x = randint(0, 620)
+            enemy.speed = randint(1, 3)
     pygame.display.update()
     clock.tick()
