2cf9e9a850526afc516c79ddc527c7975cbe76c4
fl1kers <161144727+fl1kers@users.noreply.github.com>
2024-07-27 17:39:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ background = pygame.transform.scale(pygame.image.load("castle.webp"), (700, 500)
 game = True
 clock = pygame.time.Clock()
 height = 500
-
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, image, x, y, speed, size_x,size_y):
         super().__init__()
@@ -17,8 +16,9 @@ class GameSprite(pygame.sprite.Sprite):
         self.rect.y = y
     def draw(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
-player = GameSprite('rocket.png', 20, 408, 4,50,90)
-enemies=[GameSprite('ufo.png', randint(0,620), -50, randint(2), 80, 50) for i in range(5)] #error
+player = GameSprite('rocket.png', 20, 408, 25,50,90)
+enemies=[GameSprite('ufo.png', randint(0,640), -50, randint(1,1), 80, 50) for i in range(5)] 
+bullets = []
 pygame.mixer.init()
 pygame.mixer.music.load('baby.mp3')
 pygame.mixer.music.play()
@@ -26,19 +26,36 @@ while game:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             game = False
-            
+    window.blit(background,(0,0)  )
+
+    player.draw()
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+        b = GameSprite('bullet.png', player.rect.centerx,player.rect.y, -1, 5, 20)    
+        bullets.append(b)
+        
     if keys[pygame.K_LEFT] and player.rect.x > 2:
         player.rect.x -= 1
     if keys[pygame.K_RIGHT] and player.rect.x < 645:
         player.rect.x += 1
-
-
     
 
+
+
+
+    for bullet in bullets:
+        bullet.draw()
+        bullet.rect.y += bullet.speed
+        if bullet.rect.y < -50:
+            bullets.remove(bullet)
+        for enemy in enemies:
+            if bullet.rect.colliderect(enemy.rect):
+                enemy.rect.y = -50
+                enemy.rect.x = randint(0, 620)
+                enemy.speed = randint(1, 1)
+
     
-    window.blit(background,(0,0)  )
-    player.draw()
+    
     
     for enemy in enemies:
         enemy.draw()
@@ -46,6 +63,10 @@ while game:
         if enemy.rect.y > height:
             enemy.rect.y = -50
             enemy.rect.x = randint(0, 620)
-            enemy.speed = randint(1, 3)
+            enemy.speed = randint(1, 1)
+
+
+    print(len(bullets))
+
     pygame.display.update()
-    clock.tick()
+    clock.tick(144)
